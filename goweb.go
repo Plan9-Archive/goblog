7cb2e72bd3386208d149bc94088951f4e67e6630
John Floren <john@jfloren.net>
2011-11-17 19:36:22 +0000

    now uses templates rather than generating html inline

--- goweb.go
+++ goweb.go
@@ -23,109 +23,111 @@ const (
 	blogdir = "/b/"
 )
 	
-type BlogPost struct {
+type Request struct {
 	Year	int
 	Month	int
 	Day		int
 	Post	int
 	Type	int
-
-	Title	string
-	Body	string
 }
 
-
-// Yuck yuck yuck
-func ListPosts(year, month string) (res string) {
-	res = month + "\n"
-	res = res + "<ul>\n"
-	f, err := os.Open(root + blogdir + year + "/" + month)
-	if err != nil {
-		fmt.Print(err)
-		return
-	}
-	defer f.Close()
-	fi, err := f.Readdir(0)
-	if err != nil {
-		fmt.Print(err)
-		return
-	}
-	// Step through the list of days
-	for _, info := range fi {
-		if info.IsDirectory() {
-			res = res + "<li>" + info.Name + "\n"
-			res = res + "<ul>\n"
-			day, err := os.Open(root + blogdir + year + "/" + month + "/" + info.Name)
-			if err != nil {
-				fmt.Print(err)
-				return
-			}
-			defer day.Close()
-			posts, err := day.Readdir(0)
-			if err != nil {
-				fmt.Print(err)
-				return
-			}
-			// Step through the posts under this day
-			for _, post := range posts {
-				p, err := os.Open(root + blogdir + year + "/" + month + "/" + info.Name + "/" + post.Name)
-				if err != nil {
-					fmt.Print(err)
-					return
-				}
-				defer p.Close()
-				read := bufio.NewReader(p)
-				title, _, err := read.ReadLine()
-				if err != nil {
-					res = res + "<li>" + string(title) + "\n"
-				} else {
-					return
-				}
-			}
-			res = res + "</ul>\n"
-		}
-	}
-	res = res + "</ul>\n"
-	return res
+type Archive struct {
+	Years	[]*ArchiveYear
 }
 
-func GenYear(year string) (res string) {
-	res = year + "\n"
-	res = res + "<ul>\n"
+type ArchiveYear struct {
+	Year	string
+	Posts	[]*BlogPost
+}
+
+type BlogPost struct {
+	Path	string // e.g. "/b/2011/11/16/0"
+	Title	string // e.g. "My First Post"
+	Body	string // the file converted to HTML
+}
+
+func GenYear(year string) (res []*BlogPost) {
 	f, err := os.Open(root + blogdir + year)
 	if err != nil {
 		fmt.Print(err)
 	}
 	defer f.Close()
-	fi, err := f.Readdir(0)
+	months, err := f.Readdir(0)
 	if err != nil {
 		fmt.Print(err)
 	}
-	for _, info := range fi {
-		if info.IsDirectory() {
-			res = res + "<li>" + ListPosts(year, info.Name)
+	for _, month := range months {
+		if month.IsDirectory() {
+			fmt.Printf("month = %v\n", month.Name)
+			g, err := os.Open(root + blogdir + year + "/" + month.Name)
+			if err != nil {
+				fmt.Print(err)
+				return
+			}
+			defer g.Close()
+			days, err := g.Readdir(0)
+			if err != nil {
+				fmt.Print(err)
+				return
+			}
+			// Step through the list of days
+			for _, day := range days {
+				if day.IsDirectory() {
+					fmt.Printf("day = %v\n", day.Name)
+					h, err := os.Open(root + blogdir + year + "/" + month.Name + "/" + day.Name)
+					if err != nil {
+						fmt.Print(err)
+						return
+					}
+					defer h.Close()
+					posts, err := h.Readdir(0)
+					if err != nil {
+						fmt.Print(err)
+						return
+					}
+					// Step through the posts under this day
+					for _, post := range posts {
+						fmt.Printf("post = %v\n", post.Name)
+						p, err := os.Open(root + blogdir + year + "/" + month.Name + "/" + day.Name + "/" + post.Name)
+						if err != nil {
+							fmt.Print(err)
+							return
+						}
+						defer p.Close()
+						read := bufio.NewReader(p)
+						title, _, err := read.ReadLine()
+						fmt.Printf("read title = %v, err = %v\n", title, err)
+						if err == nil {
+							fmt.Printf("appending a post with title\n")
+							res = append(res, &BlogPost{blogdir + year + "/" + month.Name + "/" + day.Name + "/" + post.Name, string(title), ""})
+							fmt.Printf("res is now %v\n", res)
+						} else {
+							fmt.Print(err)
+						}
+					}
+				}
+			}
 		}
 	}
-	res = res + "</ul>\n"
 	return res
 }
 
-func (b *BlogPost) GenArchivePage() {
-	res := "<ul>\n"
+func GenArchivePage() (res Archive) {
+	var y *ArchiveYear
 	f, _ := os.Open(root + blogdir)
 	defer f.Close()
 	fi, _ := f.Readdir(0)
 	for _, info := range fi {
 		if info.IsDirectory() {
-			res = res + "<li>" + GenYear(info.Name)
+			y = &ArchiveYear{info.Name, GenYear(info.Name)}
+			res.Years = append(res.Years, y)
 		}
 	}
-	res = res + "</ul>\n"
-	b.Body = res
+	return res
 }
 
-func NewBlogPost(path string) (b *BlogPost) {
-	b = new(BlogPost)
+func NewRequest(path string) (r *Request) {
+	r = new(Request)
 
 	splitpath := strings.Split(path, "/")
 	if path == "" {
@@ -135,22 +137,22 @@ func NewBlogPost(path string) (b *BlogPost) {
 
 	switch len(splitpath) {
 		case 4:
-			b.Post, _ = strconv.Atoi(splitpath[3])
+			r.Post, _ = strconv.Atoi(splitpath[3])
 			fallthrough
 		case 3:
-			b.Day, _ = strconv.Atoi(splitpath[2])
+			r.Day, _ = strconv.Atoi(splitpath[2])
 			fallthrough
 		case 2:
-			b.Month, _ = strconv.Atoi(splitpath[1])
+			r.Month, _ = strconv.Atoi(splitpath[1])
 			fallthrough
 		case 1:
-			b.Year, _ = strconv.Atoi(splitpath[0])
+			r.Year, _ = strconv.Atoi(splitpath[0])
 			break;
 	}
 
-	b.Type = len(splitpath)
+	r.Type = len(splitpath)
 
-	return b
+	return r
 }
 
 func BlogServer(w http.ResponseWriter, req *http.Request) {
@@ -158,29 +160,24 @@ func BlogServer(w http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path[len(blogdir):]
 	base := root + blogdir
 
-	bp := NewBlogPost(path)
+	r := NewRequest(path)
 
-	bp.Title = "foobar"
+	bp := new(BlogPost)
 
-	t := template.Must(template.ParseFile("/usr/john/goweb/blog.html"))
-
-	switch bp.Type {
+	switch r.Type {
 	case Post:
 		tmp, _ := ioutil.ReadFile(base + path)
 		bp.Body = string(tmp)
-		break
-	case Year:
-		bp.Body = GenYear(strconv.Itoa(bp.Year))
-		break
-	case Day:
-	case Month:
-		bp.Body = ListPosts(strconv.Itoa(bp.Year), strconv.Itoa(bp.Month))
+		t := template.Must(template.ParseFile("/usr/john/goweb/page.html"))
+		t.Execute(w, bp)
 		break
 	default:
-		bp.GenArchivePage()
+		archive := GenArchivePage()
+		fmt.Printf("posts = %v\n", (archive.Years[0].Posts))
+		t := template.Must(template.ParseFile("/usr/john/goweb/archive.html"))
+		t.Execute(w, archive)
 		break
 	}
-	t.Execute(w, bp)
 }
 
 func main() {
